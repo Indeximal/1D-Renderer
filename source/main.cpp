e30a83209b3ed71ee5320d6bfa915870d60c24c9
Indeximal <indeximal@gmail.com>
2017-10-08 18:16:29 +0200

    Implemented 1d perspective near and far clip plane as well as zoom

--- source/main.cpp
+++ source/main.cpp
@@ -58,18 +58,30 @@ int main() {
     b2d::Matrix3 rTDViewMat = b2d::Matrix3::translate2d(750, 400) * b2d::Matrix3::scale2d(100) * b2d::Matrix3::scale2d(1, -1);
 
     b2d::Matrix3 modelTranslation = b2d::Matrix3::translate2d(0, 0);
-    b2d::Matrix3 modelScale = b2d::Matrix3::scale2d(1);
+    b2d::Matrix3 modelScale = b2d::Matrix3::scale2d(0.8);
     b2d::Matrix3 modelRotation = b2d::Matrix3::rotate2d(0);
     b2d::Matrix3 modelMat = modelTranslation * modelRotation * modelScale;
 
-    b2d::Matrix3 cameraTranslation = b2d::Matrix3::translate2d(1.5, -2);
+    b2d::Vector2 cameraPos(2, 0);
+    float cameraRot = 3.14 / 2;
+
+    b2d::Matrix3 cameraTranslation = b2d::Matrix3::translate2d(cameraPos.x, cameraPos.y);
     b2d::Matrix3 cameraScale = b2d::Matrix3::scale2d(0.1);
-    b2d::Matrix3 cameraRotation = b2d::Matrix3::rotate2d(3.14 / 4);
+    b2d::Matrix3 cameraRotation = b2d::Matrix3::rotate2d(cameraRot);
     b2d::Matrix3 cameraMat = cameraTranslation * cameraRotation * cameraScale;
 
-    b2d::Matrix3 viewT = b2d::Matrix3::translate2d(-1.5, 2);
-    b2d::Matrix3 viewR = b2d::Matrix3::rotate2d(- 3.14 / 4);
+    b2d::Matrix3 viewT = b2d::Matrix3::translate2d(-cameraPos.x, -cameraPos.y);
+    b2d::Matrix3 viewR = b2d::Matrix3::rotate2d(-cameraRot);
     b2d::Matrix3 viewMat = viewR * viewT; // Inversed
+
+    float perspNearClip = 0.1;
+    float perspFarClip = 3;
+    float perspFOV = 90 / 180 * 3.14;
+    float zoomFactor = 0.8; //std::atan(3.14 - perspFOV)
+
+    b2d::Matrix3 perspTranslation = b2d::Matrix3::translate2d(0, -perspNearClip);
+    b2d::Matrix3 perspScale = b2d::Matrix3::scale2d(zoomFactor, 1 / (perspFarClip - perspNearClip)); //TODO x-axis
+    b2d::Matrix3 perspMat = perspScale * perspTranslation;
 
     auto lastFrame = std::chrono::high_resolution_clock::now();
     auto startTime = std::chrono::high_resolution_clock::now();
@@ -119,10 +131,16 @@ int main() {
         { // Right: Top Down Camera
             std::vector<sf::Vertex> sfVerts;
         
+            for (size_t index : indices) { // Perspective
+                b2d::Vector2 pos = vertices.at(index);
+                pos = viewMat * modelMat * pos; // VertexShader
+                b2d::Vector2 persp = rTDViewMat * perspMat * b2d::Vector2(pos.x / pos.y, pos.y);
+                sfVerts.push_back(sf::Vertex(sf::Vector2f(persp.x, persp.y), sf::Color::Green));
+            }
             for (size_t index : indices) {
                 b2d::Vector2 pos = vertices.at(index);
-                pos = rTDViewMat * viewMat * modelMat * pos; // VertexShader
-                sfVerts.push_back(sf::Vertex(sf::Vector2f(pos.x, pos.y), sf::Color::Green));
+                pos = rTDViewMat * perspMat * viewMat * modelMat * pos; // VertexShader
+                sfVerts.push_back(sf::Vertex(sf::Vector2f(pos.x, pos.y), sf::Color::Cyan));
             }
             for (size_t index : indicesGrid) {
                 b2d::Vector2 pos = verticesGrid.at(index);
