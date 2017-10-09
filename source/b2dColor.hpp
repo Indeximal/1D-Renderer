20c31ff01c6abbbc3e8c5bd8fdfe39a07c8b0f37
Indeximal <indeximal@gmail.com>
2017-10-09 16:56:09 +0200

    Added 1D rendering

--- source/b2dColor.hpp
+++ source/b2dColor.hpp
@@ -20,5 +20,32 @@ namespace b2d  // Basic 2D
         Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
         : r(r), g(g), b(b), a(a)
         {}
+
+        static Color Red()
+        {
+            static Color col(255, 0, 0);
+            return col;
+        }
+        static Color Green()
+        {
+            static Color col(0, 255, 0);
+            return col;
+        }
+        static Color Blue()
+        {
+            static Color col(0, 0, 255);
+            return col;
+        }
+        static Color White()
+        {
+            static Color col(255, 255, 255);
+            return col;
+        }
+        static Color Black()
+        {
+            static Color col(0, 0, 0);
+            return col;
+        }
     };
+    
 }
--- source/main.cpp
+++ source/main.cpp
@@ -8,21 +8,40 @@
 #include "screen.hpp"
 
 int main() {
-    b2d::Screen screen("2D Perspective", 1000, 500);
- 
+    int width = 1000;
+    int posYframe = 560;
+
+    b2d::Screen screen("2D Perspective", width, 600);
 
     b2d::Matrix3 topDownViewMat = b2d::Matrix3::translate2d(250, 250) * b2d::Matrix3::scale2d(100) * b2d::Matrix3::scale2d(1, -1);
-    b2d::Matrix3 rTDViewMat = b2d::Matrix3::translate2d(750, 400) * b2d::Matrix3::scale2d(100) * b2d::Matrix3::scale2d(1, -1);
+    b2d::Matrix3 rTDViewMat = b2d::Matrix3::translate2d(750, 400) * b2d::Matrix3::scale2d(150) * b2d::Matrix3::scale2d(1, -1);
 
+    b2d::Model gridL = b2d::Model::Grid(2.5, 0.1);
+    gridL.setColor(b2d::Color::Black());
+    b2d::Model gridR = b2d::Model::Grid(2, 2, 5, 1, 0.1);
+    gridR.setColor(b2d::Color::Black());
 
-    b2d::Model grid = b2d::Model::Grid(2.5, 2.5, 5, 2.5, 0.1);
-    grid.setColor(0, 0, 0);
-
-    b2d::Model square = b2d::Model::Square();
-    square.setColor(0, 255, 0);
-    square.setPosition(0, 0);
-    square.setScale(0.8);
-    square.setRotation(0);
+    std::vector<b2d::Model> models;
+    {
+        b2d::Model square = b2d::Model::Square();
+        square.setColor(b2d::Color::Green());
+        square.setPosition(1, 2);
+        square.setScale(0.8, 1.1);
+        square.setRotation(2);
+        models.push_back(square);
+        b2d::Model square2 = b2d::Model::Square();
+        square2.setColor(b2d::Color::Green());
+        square2.setPosition(-0.5, 0.5);
+        square2.setScale(0.4, 0.5);
+        square2.setRotation(1);
+        models.push_back(square2);
+        b2d::Model square3 = b2d::Model::Square();
+        square3.setColor(b2d::Color::Green());
+        square3.setPosition(0.6, -1.2);
+        square3.setScale(0.7);
+        square3.setRotation(3);
+        models.push_back(square3);
+    }
 
     b2d::Vector2 cameraPos(2, 0);
     float cameraRot = 3.14 / 2;
@@ -46,6 +65,9 @@ int main() {
     b2d::Matrix3 perspScale = b2d::Matrix3::scale2d(zoomFactor, 1 / (perspFarClip - perspNearClip)); //TODO x-axis
     b2d::Matrix3 perspMat = perspScale * perspTranslation;
 
+    b2d::Color frameBuffer[width];
+    float depthBuffer[width];
+
     auto lastFrame = std::chrono::high_resolution_clock::now();
     auto startTime = std::chrono::high_resolution_clock::now();
 
@@ -57,22 +79,70 @@ int main() {
         screen.clear();
 
         // Left: Top Down
-        screen.draw(square, topDownViewMat);
-        screen.draw(grid, topDownViewMat);
+        for (b2d::Model model : models)
+        {
+            screen.draw(model, topDownViewMat);
+        }
+        screen.draw(gridL, topDownViewMat);
         screen.draw(camera, topDownViewMat); 
 
-        // Right: Top Down Camera
+        // Right: Top Down Camera 
+        std::vector<b2d::Vector2> tdVerts;
         std::vector<b2d::Vector2> verts;
-        for (b2d::Vector2 vert : square.getWorldVertices()) { // Perspective
-            b2d::Vector2 pos = viewMat * vert; // VertexShader
-            b2d::Vector2 persp = rTDViewMat * perspMat * b2d::Vector2(pos.x / pos.y, pos.y);
-            verts.push_back(persp);
+        for (b2d::Model model : models)
+        {
+            for (b2d::Vector2 vert : model.getWorldVertices()) { // Perspective
+                b2d::Vector2 pos = viewMat * vert; // VertexShader
+                b2d::Vector2 persp = perspMat * b2d::Vector2(pos.x / std::abs(pos.y), pos.y);
+                verts.push_back(persp);
+                tdVerts.push_back(rTDViewMat * persp);
+            }
         }
-        screen.drawLines(verts, square.getColor());
-
-        screen.draw(square, rTDViewMat * perspMat * viewMat, b2d::Color(0, 255, 255));
-        screen.draw(grid, rTDViewMat);
+        screen.drawLines(tdVerts, b2d::Color::Red());
+        screen.draw(gridR, rTDViewMat);
         screen.draw(camera, rTDViewMat * viewMat);
+
+        for (int i = 0; i < width; i++) // clear1dFrame
+        {
+            frameBuffer[i] = b2d::Color::White();
+            depthBuffer[i] = 1;
+        }
+
+        for (int i = 0; i < verts.size(); i += 2)
+        {
+            b2d::Vector2 v1 = verts[i];
+            b2d::Vector2 v2 = verts[i+1];
+            if (v1.x > v2.x){
+                b2d::Vector2 tmp = v2;
+                v2 = v1;
+                v1 = tmp;
+            }
+            int leftEdge = (v1.x + 1) / 2 * width;
+            int rightEdge = (v2.x + 1) / 2 * width;
+            int diff = rightEdge - leftEdge;
+            
+            for (int pixel = leftEdge > 0 ? leftEdge : 0; pixel < (rightEdge < width ? rightEdge : width); pixel++)
+            {
+                float t = (pixel - leftEdge) / (float)diff;
+                if (t < 0 || t > 1) {
+                    continue;
+                }
+                float depth = v1.y * (1-t) + v2.y * t;
+                if (depth < 0 || depth > 1) {
+                    continue;
+                }
+                if (depth < depthBuffer[pixel])
+                {
+                    depthBuffer[pixel] = depth;
+                    frameBuffer[pixel] = b2d::Color::Red();
+                }
+            }
+        }
+
+        for (int i = 0; i < width; i++) // present
+        {
+            screen.setPixel(i, posYframe, frameBuffer[i]);
+        }
 
         screen.show();
 
